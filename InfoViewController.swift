304927a7250abb791c4160801e4e27f35325b0fe
Maria Nesterova <ridebyhorse@gmail.com>
2023-11-14 16:02:01 +0300

    Created a base navigation

---
+++ InfoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  InfoViewController.swift
+//  HabitHive
+//
+//  Created by Мария Нестерова on 14.11.2023.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .sunrise
+    }
+
+}
